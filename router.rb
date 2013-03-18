51820a3ea6a3e99d08c0b0f2f56d4427afe9863b
Andrew Mawer <andrew@bitmakerlabs.com>
2013-03-18 10:36:47 -0400

    Action/index methods

--- router.rb
+++ router.rb
@@ -6,5 +6,15 @@ if ARGV.empty?
 	puts "USAGE: bla bla bla"
 else
 	puts "Yay! You passed in some arguments"
+
+	if ARGV[0] == "index"
+		puts "Action: index"
+	elsif ARGV[0] == "show"
+		puts "Action: show"
+	else
+		puts "Unrecognized action. Please refer to USAGE"
+	end
+
+
 end
 
