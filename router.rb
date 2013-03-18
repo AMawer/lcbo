60898cd98e76785feffb4248ca8bad48c70cba8f
Andrew Mawer <andrew@bitmakerlabs.com>
2013-03-18 10:32:06 -0400

    using ARGV

---
+++ router.rb
@@ -0,0 +1,10 @@
+# Access parameters passed into the app
+
+# if there are no arguments provided
+
+if ARGV.empty?
+	puts "USAGE: bla bla bla"
+else
+	puts "Yay! You passed in some arguments"
+end
+
