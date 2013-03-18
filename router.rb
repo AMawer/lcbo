fa42711e361d90a3cdbfdd021fdc2de1815ed902
Andrew Mawer <andrew@bitmakerlabs.com>
2013-03-18 10:45:58 -0400

    Created the HtmlGenerator class

--- router.rb
+++ router.rb
@@ -1,18 +1,16 @@
-# Access parameters passed into the app
-
-# if there are no arguments provided
+require_relative 'html_generator'
 
 if ARGV.empty?
 	puts "USAGE: bla bla bla"
 else
 	puts "Yay! You passed in some arguments"
-
+	generator = HtmlGenerator.new
 	if ARGV[0] == "index"
-		puts "Action: index"
+		generator.index
 	elsif ARGV[0] == "show"
-		puts "Action: show"
+		generator.show
 	else
-		puts "Unrecognized action. Please refer to USAGE"
+		
 	end
 
 
